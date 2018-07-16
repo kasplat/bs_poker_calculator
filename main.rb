5a8cac065806f834d9a3c121a67aabb2f1cd2dc6
Kesavan Kushalnagar <Kesavan_Kushalnagar@symantec.com>
2018-07-16 10:23:21 -0700

    init commit

---
+++ main.rb
@@ -0,0 +1,16 @@
+require 'rubycards'
+include RubyCards
+
+def two_of_a_kind(hand)
+    pair_dict = {}
+    hand.each do |card|
+        if pair_dict[card.rank]
+            return true
+        end
+        pair_dict[card.rank] = True
+    end
+    return false
+end
+
+pair_aces = Hand.new([Card.new('Ace', 'Hearts'), Card.new('Ace', 'Clubs')])
+puts two_of_a_kind(pair_aces)
