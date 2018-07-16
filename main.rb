02c661f80657d36a2bb6b5716b7d85447aa0d44b
Kesavan Kushalnagar <Kesavan_Kushalnagar@symantec.com>
2018-07-16 11:45:58 -0700

    n of a kind

--- main.rb
+++ main.rb
@@ -1,13 +1,44 @@
 require 'rubycards'
 include RubyCards
 
-def two_of_a_kind(hand)
-    pair_dict = {}
+
+def n_of_a_kind(hand, n)
+    hand_dict = {}
+    puts hand
     hand.each do |card|
-        if pair_dict[card.rank]
-            return true
+        if hand_dict[card.rank] == n - 1
+            return [true, card.rank]
+        else
+            if hand_dict[card.rank]
+                hand_dict[card.rank] += 1
+            else
+                hand_dict[card.rank] = 1
+            end
         end
-        pair_dict[card.rank] = true
+    end
+    return [false, '0']
+end
+
+def n_pair(hand, n)
+    hand_dict = {}
+    hand.each do |card|
+        if hand_dict[card.rank]
+            hand_dict[card.rank] += 1
+        else
+            hand_dict[card.rank] = 1
+        end
+    end
+
+    pairs = []
+    hand_dict.each do |key, value|
+        if value >= 2
+            pairs.push(key)
+            if pairs.length == n
+                return pairs
+            end
+        end
     end
     return false
+
 end
+
--- unit_test_hands.rb
+++ unit_test_hands.rb
@@ -5,15 +5,39 @@ class TestPair < Test::Unit::TestCase
     
     def setup
         @pair_aces = Hand.new([Card.new('Ace', 'Hearts'), Card.new('Ace', 'Clubs')])
-        @three_in_a_row = Hand.new([Card.new('1', 'Hearts'), Card.new('Ace', 'Hearts'), Card.new('Queen', 'Hearts')])
+        @three_random = Hand.new([Card.new('1', 'Hearts'), Card.new('Ace', 'Hearts'), Card.new('Queen', 'Hearts')])
+        @pair_aces_and_fives = @pair_aces.cards.push(Card.new(5, 'Hearts'), Card.new(5, 'Clubs'))
+        @triple_aces = @pair_aces.cards.push(Card.new('Ace', 'Spades'))
+        puts @pair_aces
+        puts @three_random
     end
 
     def test_pair_true
-        assert_equal(true, two_of_a_kind(@pair_aces))
+        assert_equal([true, 'Ace'], n_of_a_kind(@pair_aces, 2))
     end
 
     def test_pair_false
-        assert_equal(false, two_of_a_kind(@three_in_a_row))
+        assert_equal([false, '0'], n_of_a_kind(@three_random, 2))
+    end
+
+    def test_1_pair_true
+        assert_equal(['Ace'], n_pair(@pair_aces, 1))
+    end
+
+    def test_1_pair_false
+        assert_equal(false, n_pair(@three_random, 1))
+    end
+
+    def test_2_pair_true
+        assert_equal(['Ace', '5'], n_pair(@pair_aces_and_fives, 2))
+    end
+
+    def test_triple_true
+        assert_equal([true, 'Ace'], n_of_a_kind(@triple_aces, 3))
+    end
+
+    def test_triple_false
+        assert_equal([false, '0'], n_of_a_kind(@pair_aces_and_fives, 3))
     end
 
 end
