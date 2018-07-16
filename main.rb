189c60967d78248b672bb014ea0b6e69ae4bf34b
Kesavan Kushalnagar <Kesavan_Kushalnagar@symantec.com>
2018-07-16 13:56:36 -0700

    added flush & full house

--- main.rb
+++ main.rb
@@ -41,3 +41,62 @@ def n_pair(hand, n)
 
 end
 
+def straight(hand)
+    hand.sort!()
+    recent = "0"
+    current_length = 0
+    hand.each do |card|
+        if recent == card.rank.to_i - 1
+            current_length += 1
+            if current_length == 5
+                return [true, card.rank]
+            end
+        else
+            current_length = 1
+        end
+        recent = card.rank.to_i
+    end
+    return false
+end
+
+def flush(hand)
+    hand_dict = {}
+    hand.each do |card|
+        if hand_dict[card.suit] == 4
+            return [true, card.suit]
+        else
+            if hand_dict[card.suit]
+                hand_dict[card.suit] += 1
+            else
+                hand_dict[card.suit] = 1
+            end
+        end
+    end
+    return false
+end
+
+def full_house(hand)
+    hand_dict = {}
+    hand.each do |card|
+        if hand_dict[card.rank]
+            hand_dict[card.rank] += 1
+        else
+            hand_dict[card.rank] = 1
+        end
+    end
+
+    three_of_a_kind = false
+    two_of_a_kind = false
+    hand_dict.each do |key, value|
+        if value >= 3 and not three_of_a_kind
+            three_of_a_kind = key
+        elsif value >= 2
+            two_of_a_kind = key
+        end 
+    end
+    if three_of_a_kind and two_of_a_kind
+        return [three_of_a_kind, two_of_a_kind]
+    else
+        return false
+    end
+end
--- unit_test_hands.rb
+++ unit_test_hands.rb
@@ -8,6 +8,10 @@ class TestPair < Test::Unit::TestCase
         @three_random = Hand.new([Card.new(3, 'Hearts'), Card.new('Ace', 'Hearts'), Card.new('Queen', 'Hearts')])
         @pair_aces_and_fives = @pair_aces.cards + [Card.new(5, 'Hearts'), Card.new(5, 'Clubs')]
         @triple_aces = @pair_aces.cards + [Card.new('Ace', 'Spades')]
+        @small_straight = Hand.new([Card.new(3), Card.new(4), Card.new(5), Card.new(6)])
+        @straight = @small_straight.cards + [Card.new(7)]
+        @four_spades_3_hearts = @three_random.cards + @small_straight.cards
+        @three_aces_two_fives = @pair_aces_and_fives + [Card.new('Ace', 'Spades')]
     end
 
     def test_pair_true
@@ -40,6 +44,30 @@ class TestPair < Test::Unit::TestCase
 
     def test_triple_false
         assert_equal([false, '0'], n_of_a_kind(@pair_aces_and_fives, 3))
+    end
+
+    def test_straight_true
+        assert_equal([true, '7'], straight(@straight))
+    end
+    
+    def test_straight_false
+        assert_equal(false, straight(@small_straight))
+    end
+
+    def test_flush_true
+        assert_equal([true, "Spades"], flush(@straight))
+    end
+
+    def test_flush_false
+        assert_equal(false, flush(@four_spades_3_hearts))
+    end
+
+    def test_full_house_true
+        assert_equal(["Ace", "5"], full_house(@three_aces_two_fives))
+    end
+
+    def test_full_house_false
+        assert_equal(false, full_house(@four_spades_3_hearts))
     end
 
 end
